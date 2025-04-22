c1520eaeed17b7162951eb5ac68f0406174b22a5
jorgericardoacaldas <ricardo10caldas@gmail.com>
2025-04-22 09:19:42 -0300

    Add scan for windows

--- snap_listusb/UsbDeviceScanner.cs
+++ snap_listusb/UsbDeviceScanner.cs
@@ -3,60 +3,44 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Management;
+
 
 public class UsbDeviceScanner
 {
+    
     public List<UsbDeviceInfo> GetConnectedUsbDevices()
-{
-    var devices = new List<UsbDeviceInfo>();
-
-    var process = new Process
     {
-        StartInfo = new ProcessStartInfo
+        var devices = new List<UsbDeviceInfo>();
+
+        var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub");
+
+        foreach (ManagementObject device in searcher.Get())
         {
-            FileName = "ioreg",
-            Arguments = "-p IOUSB -l",
-            RedirectStandardOutput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
+            devices.Add(new UsbDeviceInfo
+            {
+                VendorId = ExtractVendorId(device["DeviceID"]?.ToString()),
+                ProductId = ExtractProductId(device["DeviceID"]?.ToString()),
+                VendorName = device["Manufacturer"]?.ToString() ?? "Unknown",
+                ProductName = device["Name"]?.ToString() ?? "Unknown",
+                SerialNumber = device["PNPDeviceID"]?.ToString() ?? "N/A"
+            });
         }
-    };
 
-    process.Start();
-    string output = process.StandardOutput.ReadToEnd();
-    process.WaitForExit();
-
-    var blocks = Regex.Split(output, @"\+-o ");
-
-    foreach (var block in blocks)
-    {
-        if (!block.Contains("idVendor") || !block.Contains("idProduct"))
-            continue;
-
-        var device = new UsbDeviceInfo();
-
-        var idVendorMatch = Regex.Match(block, @"""idVendor""\s*=\s*(\d+)");
-        if (idVendorMatch.Success)
-            device.VendorId = int.Parse(idVendorMatch.Groups[1].Value);
-
-        var idProductMatch = Regex.Match(block, @"""idProduct""\s*=\s*(\d+)");
-        if (idProductMatch.Success)
-            device.ProductId = int.Parse(idProductMatch.Groups[1].Value);
-
-        var productMatch = Regex.Match(block, @"""(?:USB Product Name|kUSBProductString)""\s*=\s*""([^""]+)""");
-        device.ProductName = productMatch.Success ? productMatch.Groups[1].Value : "Unknown";
-
-        var vendorMatch = Regex.Match(block, @"""(?:USB Vendor Name|kUSBVendorString)""\s*=\s*""([^""]+)""");
-        device.VendorName = vendorMatch.Success ? vendorMatch.Groups[1].Value : "Unknown";
-
-        var serialMatch = Regex.Match(block, @"""(?:USB Serial Number|kUSBSerialNumberString)""\s*=\s*""([^""]+)""");
-        device.SerialNumber = serialMatch.Success ? serialMatch.Groups[1].Value : "N/A";
-
-        devices.Add(device);
+        return devices;
     }
 
-    return devices;
-}
+    private int ExtractVendorId(string deviceId)
+    {
+        var match = System.Text.RegularExpressions.Regex.Match(deviceId ?? "", @"VID_([0-9A-F]{4})", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+        return match.Success ? Convert.ToInt32(match.Groups[1].Value, 16) : 0;
+    }
+
+    private int ExtractProductId(string deviceId)
+    {
+        var match = System.Text.RegularExpressions.Regex.Match(deviceId ?? "", @"PID_([0-9A-F]{4})", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+        return match.Success ? Convert.ToInt32(match.Groups[1].Value, 16) : 0;
+    }
 
 
     public void SaveToCsv(List<UsbDeviceInfo> devices, string filePath)
--- snap_listusb/model/UsbDeviceInfo.cs
+++ snap_listusb/model/UsbDeviceInfo.cs
@@ -1,8 +1,8 @@
 public class UsbDeviceInfo
-{
-    public string ProductName { get; set; }
-    public string VendorName { get; set; }
-    public int VendorId { get; set; }
-    public int ProductId { get; set; }
-    public string SerialNumber { get; set; }
-}
+    {
+        public string VendorName { get; set; }
+        public string ProductName { get; set; }
+        public int VendorId { get; set; }
+        public int ProductId { get; set; }
+        public string SerialNumber { get; set; }
+    }
